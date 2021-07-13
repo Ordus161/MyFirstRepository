7d5ea48eb6aa7ee97b4d0ad4f32894bb5a01c67a
Maxim Bondaruk <makc1992@list.ru>
2021-07-13 21:14:41 +0300

    array template

---
+++ Array/Array.cpp
@@ -0,0 +1,25 @@
+﻿#include "ArrayTemp.h"
+#include "ArrayTemp.cpp"
+
+
+
+int main()
+{
+	try 
+	{
+		ArrayTemp <int> myMass(5);
+		//cout << myMass;
+		myMass.Insert(1, 0);
+		myMass.Insert(2, 1);
+		myMass.Insert(3, 2);
+		cout << myMass;
+		//myMass.Insert(2, 0);
+		//cout << myMass;
+		cout << myMass.Remove(2) << endl;
+		cout << myMass;
+	}
+	catch (exception& exp)
+	{
+		cout << exp.what() << endl;
+	}
+}
---
+++ Array/ArrayTemp.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class ArrayTemp
+{
+private:
+	T* mass = NULL;
+	int size = 0;
+public:
+	ArrayTemp(int = 0);
+	friend ostream& operator <<(ostream& out, ArrayTemp<T>& arr)
+	{
+        try
+        {
+            if (arr.mass == NULL)
+                throw invalid_argument("null mass argument");
+            for (int i = 0; i < arr.size; i++)
+            {
+                cout << arr.mass[i] << "\t";
+            }
+            out << endl;
+            return out;
+        }
+        catch (exception& exp)
+        {
+            cout << "Input error:" << exp.what() << endl;
+        }
+	}
+
+	void Insert(T value, int index =0);
+	T Remove(int index);
+};
+
